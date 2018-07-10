49f388bd143c6be57efdb368d3b62f5a6c8fc7bb
Jane Kim <kimjane7@msu.edu>
2018-07-10 17:03:32 -0400

    almost done with magnus expansion?

--- src/srg.cpp
+++ src/srg.cpp
@@ -1,113 +1,167 @@
-#include "diag.h"
+#include <iostream>
+#include <cmath>
+#include <string> 
+#include <fstream>
+#include <iomanip>
+#include <armadillo>
+#include "time.h"
 
-SRG::SRG(mat &H, int N, double smax, double ds){
+using namespace std;
+using namespace arma;
 
-	H0_ = H;
-	N_ = N;
-	smax_ = smax;
-	ds_ = ds;
-	dsI_ = zeros<mat>(N_,N_);
-	for(int i = 0; i < N_; ++i) dsI(i,i) = ds;
-	for(int n = 0; n < max_; ++n) prefactor_[n] = B_[n]/factorial(n);
-}
 
-void SRG::display_H(){
 
-	cout << "\ns = " << s_ << ":\n" << endl;
+static const int Bmax = 22;
+static const vec B = {1.0, -1/2.0, 1/6.0, 0.0, -1/30.0, 
+	                  0.0, 1/42.0, 0.0, -1/30.0, 0.0, 5/66.0, 
+	                  0.0, -691/2730.0, 0.0, 7/6.0, 0.0, -3617/510.0, 
+	                  0.0, 43867/798.0, 0.0, -174611/330, 0.0};
+
+// displays NxN matrix
+void display(mat& A, int N){
 	
-	for(int i = 0; i < N_; i++){
+	for(int i = 0; i < N; i++){
 		cout << "[";
-		for(int j = 0; j < N_; j++){
-			cout << setw(10) << setprecision(3) << H_(i,j);
+		for(int j = 0; j < N; j++){
+			cout << setw(10) << setprecision(3) << A(i,j);
 		}
 		cout << "]" << endl;
 	}
 	cout << endl;	
 }
 
-mat SRG::commutator(mat& A, mat& B){
-	
-	return A*B-B*A;
-}
-
-void SRG::split(){
-
-	Hd_ = diagmat(H_);
-	Hod_ = H_-Hd_;
-}
-
-mat derivative_direct(){
-
-
-}
-
-// returns H_{n+1}
-mat SRG::RK4(){
-
-	
-	mat k1 =  
-}
-
-void SRG::direct_integration(){
-
-	H_ = H0_;
-	double sprint = smax_
-
-	for(s_ = 0.0; s_ <= smax_; s_ += ds_){
-
-		// split H into diag and off-diag parts
-		split();
-
-		// calculate generator eta and derivative dH/ds
-		eta_ = commutator(Hd_,Hod_);
-		derivative_ = commutator(eta_,H_);
-
-		// forward euler
-		H_ = H_+ds_*derivative_;
-
-
-		// RK4
-		H_ = RK4();
-	}
-
-	display_H();
-}
-
-int SRG::factorial(int n){
+int factorial(int n){
 
 	int factorial = 1;
 
 	if(n > 1){
 		for(int i = 2; i <= n; ++i) factorial *= i;
 	}
-	else return factorial;
+	
+	return factorial;
 }
 
-mat SRG::nested_commutator(int n){
+mat commutator(mat A, mat B){
+	
+	return A*B-B*A;
+}
 
-	if(n == 0) return eta_;
+mat nested_commutator(mat A, mat B, int n){
+
+	if(n == 0) return B;
 	else{
-		mat ad = eta_;
-		for(int i = 0; i < n; ++i) ad = commutator(omega_,ad);
+		mat ad = B;
+		for(int i = 0; i < n; ++i) ad = commutator(A,ad);
 		return ad;
 	}
 }
 
-mat SRG::derivative_magnus(){
+// returns A after solving dA/ds=derivative(A(s),s)
+// later add display statements for various s
+mat RK4(mat A0, int N, double smax, double ds, mat (*derivative)(mat,mat)){
 
-	int n = 0;
-	double tolerance = 1.0E-5;
-	mat sum, summand; 
+	mat k1, k2, k3, k4;
 
-	do{
- 		summand = prefactor_[n]*nested_commutator(n);
- 		sum += summand;
-	}while(summand > tolerance);
+	// this is a dummy variable if srg_derivative() is used
+	mat Omega  = zeros<mat>(N,N);
 
-	derivative_ = sum;
+	// algorithm
+	mat Atemp = A0;
+	for(double s = 0; s <= smax; s += ds){
+
+		k1 = ds*(*derivative)(Atemp,Omega);
+		k2 = ds*(*derivative)(Atemp+0.5*k1,Omega);
+		k3 = ds*(*derivative)(Atemp+0.5*k2,Omega);
+		k4 = ds*(*derivative)(Atemp+k3,Omega);
+
+		Atemp += (k1+2.0*k2+2.0*k3+k4)/6.0;
+	}
+
+	return Atemp;
 }
 
-void SRG::magnus(){
-	
+// generator
+mat get_eta(mat H){
 
+	// split H into diag and off-diag parts
+	mat Hd = diagmat(H);
+	mat Hod = H-Hd;
+
+	// calculate generator
+	return commutator(Hd,Hod);	
+}
+
+// dH/ds 
+// need dummy to pass function as argument in RK4
+mat srg_derivative(mat H, mat dummy){
+
+	return commutator(get_eta(H),H);
+}
+
+mat srg(mat& H0, int N, double smax, double ds){
+
+	return RK4(H0, N, smax, ds, srg_derivative);
+
+}
+
+// dOmega/ds
+mat magnus_derivative(mat H, mat Omega){
+
+	double tolerance = 1.0E-5;
+	mat eta = get_eta(H);
+
+	// only one non-zero Bernoulli number
+	mat sum = B[1]*nested_commutator(Omega,eta,1);
+
+	// add for all even k
+	for(int k = 0; k < Bmax; k += 2){
+		sum = B[k]*nested_commutator(Omega,eta,k)/factorial(k);
+	}
+
+	return sum;
+
+}
+
+mat magnus(mat& H0, int N, double smax, double ds){
+
+	return RK4(H0, N, smax, ds, magnus_derivative);
+
+}
+
+
+int main(int argc, char *argv[]){
+
+	double d = 1;
+	double g = atof(argv[1]);
+	double smax = atof(argv[2]);
+	double ds = atof(argv[3]);
+
+	// set up Hamiltonian
+	mat H0 = zeros<mat>(6,6);
+	for(int i = 0; i < 6; ++i){
+		for(int j = 0; j < 6; ++j){
+			if(i+j != 5) H0(i,j) = -0.5*g; 
+		}
+	}
+	H0(0,0) = 2*d-g;
+	H0(1,1) = 4*d-g;
+	H0(2,2) = 6*d-g;
+	H0(3,3) = 6*d-g;
+	H0(4,4) = 8*d-g;
+	H0(5,5) = 10*d-g;
+
+	cout << "\nOriginal Hamiltonian:\n" << endl;
+	display(H0, 6);	
+
+	mat H = srg(H0, 6, smax, ds);
+
+	cout << "\nDiagonalized Hamiltonian (SRG)\n" << endl;
+	display(H, 6);
+
+	H = magnus(H0, 6, smax, ds);
+
+	cout << "\nDiagonalized Hamiltonian (SRG w/ Magnus Expansion)\n" << endl;
+	display(H, 6);
+
+	return 0;
 }
--- src/srg.h
+++ src/srg.h
@@ -1,52 +1,20 @@
-#ifndef SRG_H
-#define SRG_H
-
 #include <iostream>
 #include <cmath>
 #include <string> 
 #include <fstream>
 #include <iomanip>
 #include <armadillo>
-#include <vector>
 #include "time.h"
 
 using namespace std;
 using namespace arma;
 
-const double pi = 4.0*atan(1.0);
-
-class SRG{
-
-public:
-
-	static const int max_ = 22;
-	static const vector<double> B_{1.0, -1/2.0, 1/6.0, 0.0, -1/30.0, 
-		                  0.0, 1/42.0, 0.0, -1/30.0, 0.0, 5/66.0, 
-		                  0.0, -691/2730.0, 0.0, 7/6.0, 0.0, -3617/510.0, 
-		                  0.0, 43867/798.0, 0.0, -174611/330, 0.0}
-    
-    vector<double> prefactor_;
-    int N_;
-    double s_, smax_, ds_;
-
-    mat H0_, H_, Hd_, Hod_;
-    mat eta_, omega_, derivative_;
 
 
-	SRG(mat &H, int N, double smax, double ds);
-	~SRG(){}
-
-	void display_H();
-	mat commutator(mat& A, mat& B);
-
-	void split();
-	void direct();
-	int factorial(int n);
-	mat nested_commutator(int n);
-	void derivative();
-	void magnus();
-
-};
+static const int Bmax = 22;
+static const vec B = {1.0, -1/2.0, 1/6.0, 0.0, -1/30.0, 
+	                  0.0, 1/42.0, 0.0, -1/30.0, 0.0, 5/66.0, 
+	                  0.0, -691/2730.0, 0.0, 7/6.0, 0.0, -3617/510.0, 
+	                  0.0, 43867/798.0, 0.0, -174611/330, 0.0}
 
 
-#endif
